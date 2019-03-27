734dc5120fe16feaad1ad6c46caaabd696fcc6a7
wangxiaobin <xiaobin.wang@marketin.cn>
2019-03-27 22:03:00 +0800

    init

---
+++ src/main/java/com/example/demo/helloController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO 描述:
+ *
+ * @author xiaobin.wang@marketin.cn
+ * @create 2019-03-27 21:45
+ * @since 2.18
+ */
+@RestController
+public class helloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello world";
+    }
+}
